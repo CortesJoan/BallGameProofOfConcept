b3c9403d06e90713764841bb11b9903c45faa291
Cortés <joan.cortes.7e3@itb.cat>
2021-02-10 19:54:29 +0100

    Initial commit try 15

---
+++ Stand assets/Assets/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public int score;
+    public Transform endText;
+    public Text scoreText;
+
+    public int numPickUpItems;
+    // Start is called before the first frame update
+    void Start()
+    { 
+        UpdateUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScore(int score)
+    {
+        numPickUpItems--;
+        this.score += score;
+        UpdateUI();
+        
+    }
+
+    private void UpdateUI()
+    {
+        scoreText.GetComponent<Animation>().Play();
+        scoreText.text = "Score " + score;
+        if (numPickUpItems==0)
+        {
+            endText.gameObject.SetActive(true);
+        }
+    }
+    
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void EndGame()
+    {
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+			Application.Quit();
+#endif
+    }
+}
---
+++ Stand assets/Assets/SceneController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    public float factor;
+
+    private bool inpuInThisFrame;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInputs();
+    }
+    public void HandleInputs()
+    {
+        Quaternion rotation = this.transform.rotation;
+        if (Input.GetKey(KeyCode.W))
+        {
+            rotation= new Quaternion(rotation.x + factor, rotation.y , rotation.z,1);
+        
+            this.transform.rotation = rotation;
+            inpuInThisFrame = true;
+        } 
+        if (Input.GetKey(KeyCode.S)){
+          rotation=new Quaternion(rotation.x - factor, rotation.y , rotation.z,1); 
+          inpuInThisFrame = true;
+        }
+        
+        if (Input.GetKey(KeyCode.A)){
+            rotation= new Quaternion(rotation.x , rotation.y , rotation.z+factor,1);
+            inpuInThisFrame = true;
+        } 
+        if (Input.GetKey(KeyCode.D)){
+            
+            rotation= new Quaternion(rotation.x , rotation.y , rotation.z-factor,1);
+
+            inpuInThisFrame = true;
+
+        }
+
+        if (inpuInThisFrame)
+        {
+            rb.MoveRotation(rotation.normalized);
+            inpuInThisFrame = false;
+        }
+        
+
+    }
+}
